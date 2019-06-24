c6d7e77b656964a5e451c70f6ff5c57087f68dc7
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-24 17:36:43 +0100

    feature: Add Login Form organism to App root

--- serve-soup-inventory-mgt/src/App.js
+++ serve-soup-inventory-mgt/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import GenericHeader from './components/atoms/Heading'
+import LoginForm from './components/organisms/LoginForm';
 
 function App() {
+
   return (
     <>
-    <div>Serve Soup!</div>
-    <GenericHeader fontSize='80px' color='black'>Sign In</GenericHeader>
+      <LoginForm />
     </>
   );
 }
