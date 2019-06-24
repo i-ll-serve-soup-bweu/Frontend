4cf7b12486c3db60e5e235ee9a9026ea4a1d021a
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-24 20:37:09 +0000

    feature: Add style and types for button

--- serve-soup-inventory-mgt/src/components/atoms/Button/index.js
+++ serve-soup-inventory-mgt/src/components/atoms/Button/index.js
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
-  padding: 10px;
+  background: transparent;
+  border-radius: 3px;
+  border: 0.5px solid #6cb043;
+  color: #6cb043;
+  margin: 0 1em;
+  padding: 0.25em 1em;
 `
 export default Button;
