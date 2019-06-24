11dcdf8c21a7132aea744bb1a4d0a4bd71b31760
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-24 10:02:55 +0000

    feature: Create initial action types

---
+++ serve-soup-inventory-mgt/src/actions/index.js
@@ -0,0 +1,8 @@
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+export const GET_INVENTORY = 'GET_INVENTORY';
+export const GET_KITCHEN = 'GET_KITCHEN';
+export const LOADING_USER = 'LOADING_USER';
+export const LOADING_INVENTORY = 'LOADING_INVENTORY';
+export const LOADING_KITCHEN = 'LOADING_KITCHEN';
+export const ERROR = 'ERROR';
