d83be8816d7ee72ef1989951c0960d2471c013b4
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-24 11:27:10 +0000

    feature: Add action dispatchers for logging in and signing up

--- serve-soup-inventory-mgt/src/actions/index.js
+++ serve-soup-inventory-mgt/src/actions/index.js
@@ -1,3 +1,7 @@
+import axios from 'axios';
+
+const SoupApiURL = 'https://be-soup.herokuapp.com';
+
 export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
 export const GET_INVENTORY = 'GET_INVENTORY';
@@ -39,4 +43,25 @@ export const genericAction = (type, payload) => {
     type,
     payload,
   }
+}
+
+export const doSignUp = (user) => dispatch => {
+  dispatch(genericAction(LOADING_USER, true));
+  const { username, password, name, lastName, type } = user;
+  axios.post(`${SoupApiURL}/register`, {username, password, name, lastName, type})
+    .then(() => dispatch(doLogIn(user)))
+    .catch(error => dispatch(genericAction(ERROR, error.message)))
+    .finally(() => dispatch(genericAction(LOADING_USER, false)))
+}
+
+export const doLogIn = (user) => dispatch => {
+  dispatch(genericAction(LOADING_USER, true));
+  const { username, password } = user;
+  axios.post(`${SoupApiURL}/login`, {username, password})
+    .then(response => {
+      dispatch(signUpOrLogIn(response))
+      localStorage.setItem('soupUserToken', response.token)
+    })
+    .catch(error => dispatch(genericAction(ERROR, error.message)))
+    .finally(() => dispatch(genericAction(LOADING_USER, false)))
 }
