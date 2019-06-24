26b90c3d2c89fd5eb582570234876e67322ea723
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-24 19:13:18 +0100

    feature: Add React-Router for routing add Route for SignUp add Route for Login

--- serve-soup-inventory-mgt/src/App.js
+++ serve-soup-inventory-mgt/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import LoginForm from './components/organisms/LoginForm';
+import SignUpForm from './components/organisms/SignUpForm';
 
 function App() {
 
   return (
-    <>
-      <LoginForm />
-    </>
+    <Router>
+      <Route 
+        exact
+        path='/login'
+        component={LoginForm}
+      />
+      <Route 
+        exact
+        path='/signup'
+        component={SignUpForm}
+      />
+    </Router>
   );
 }
 
