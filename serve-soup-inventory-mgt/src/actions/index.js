b4a77d03fad0e0344274198da1b336144489af81
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-24 10:45:59 +0000

    feature: Add action creators

--- serve-soup-inventory-mgt/src/actions/index.js
+++ serve-soup-inventory-mgt/src/actions/index.js
@@ -1,4 +1,3 @@
-export const SIGNUP = 'SIGNUP';
 export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
 export const GET_INVENTORY = 'GET_INVENTORY';
@@ -7,3 +6,37 @@ export const LOADING_USER = 'LOADING_USER';
 export const LOADING_INVENTORY = 'LOADING_INVENTORY';
 export const LOADING_KITCHEN = 'LOADING_KITCHEN';
 export const ERROR = 'ERROR';
+
+export const signUpOrLogIn = (user) => {
+  return {
+    type: LOGIN,
+    payload: user,
+  }
+}
+
+export const logOut = () => {
+  return {
+    type: LOGOUT,
+  }
+}
+
+export const getInventory = (inventory) => {
+  return {
+    type: GET_INVENTORY,
+    payload: inventory,
+  }
+}
+
+export const getKitchen = (kitchen) => {
+  return {
+    type: GET_KITCHEN,
+    payload: kitchen,
+  }
+}
+
+export const genericAction = (type, payload) => {
+  return {
+    type,
+    payload,
+  }
+}
