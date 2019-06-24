a1d4590fb257e987fbb48f45757356ea53ef7ff3
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-24 20:37:41 +0000

    feature: Add input style and props

--- serve-soup-inventory-mgt/src/components/atoms/Input/index.js
+++ serve-soup-inventory-mgt/src/components/atoms/Input/index.js
@@ -1,6 +1,45 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Input = styled.input`
-  
+  border: 0.5px solid black;
+  border-radius: 5px;
+  outline: none;
+  padding: 0 5px;
+
+  ${props => props.register && css`
+    height: 30px;
+    width: 17em;
+
+    :hover {
+      border: 0.5px solid grey;
+    }
+  `}
+
+  ${props => props.small && css`
+    height: 1.8em;
+    width: 1.7em;
+
+    :hover {
+      border: 0.5px solid grey;
+    }
+  `}
+
+  ${props => props.medium && css`
+    height: 1.8em;
+    width: 5em;
+
+    :hover {
+      border: 0.5px solid grey;
+    }
+  `}
+
+  ${props => props.large && css`
+    height: 1.8em;
+    width: 10em;
+
+    :hover {
+      border: 0.5px solid grey;
+    }
+  `}
 `
 export default Input;
