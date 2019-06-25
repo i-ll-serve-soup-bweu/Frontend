66159026a204fc3648cce171264bb9dc16fe8ca5
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-25 11:17:49 +0000

    feature: Add primary and secondary button

--- serve-soup-inventory-mgt/src/components/atoms/Button/index.js
+++ serve-soup-inventory-mgt/src/components/atoms/Button/index.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const StyledButton = styled.button`
   background: transparent;
@@ -7,5 +7,17 @@ const StyledButton = styled.button`
   color: #6cb043;
   margin: 0 1em;
   padding: 0.25em 1em;
+
+  ${props => props.primary && css`
+    font-size: 0.8em;
+    padding: 0.45em 8em;
+  `}
+
+  ${props => props.secondary && css`
+    background: #8DBD53;
+    color: white;
+    font-size: 0.8em;
+    padding: 0.4em 3.5em;
+  `}
 `
 export default StyledButton;
--- serve-soup-inventory-mgt/src/components/atoms/Button/index.stories.js
+++ serve-soup-inventory-mgt/src/components/atoms/Button/index.stories.js
@@ -6,5 +6,6 @@ import { action } from '@storybook/addon-actions';
 import StyledButton from './index';
 
 storiesOf('Button', module)
-  .add('primary', () => <StyledButton onClick={action('clicked')}>Sign In</StyledButton>)
+  .add('primary', () => <StyledButton primary onClick={action('clicked')}>Sign In</StyledButton>)
+  .add('secondary', () => <StyledButton secondary onClick={action('clicked')}>Save</StyledButton>)
 ;
