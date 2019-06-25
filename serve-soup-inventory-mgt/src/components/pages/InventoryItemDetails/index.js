5cdd94bd39739d351125169e88259fe1807c2e35
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-25 08:39:12 +0100

    feature: Create app main pages create initial empty pages for routing

---
+++ serve-soup-inventory-mgt/src/components/pages/InventoryItemDetails/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GenericHeader from '../../atoms/Heading';
+
+export default function InventoryItemDetails() {
+  return (
+    <div>
+      <GenericHeader>
+        Inventory Item Details
+      </GenericHeader>
+    </div>
+  );
+}
