2944a4f837d8ce46ed24e422688c1996c12bfb34
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-25 19:38:36 +0000

    chore: Remove connect import

--- serve-soup-inventory-mgt/src/components/organisms/AppNav/index.js
+++ serve-soup-inventory-mgt/src/components/organisms/AppNav/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { StyledLogoText, StyledButton } from '../../atoms';
