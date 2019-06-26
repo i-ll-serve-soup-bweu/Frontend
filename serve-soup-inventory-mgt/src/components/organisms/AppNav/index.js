7ca285d081f1ac026031a6f619ee50864fb02952
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-26 09:20:22 +0000

    feature: Set condition on how NavBar is rendered

--- serve-soup-inventory-mgt/src/components/organisms/AppNav/index.js
+++ serve-soup-inventory-mgt/src/components/organisms/AppNav/index.js
@@ -1,36 +1,84 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 import { StyledLogoText, StyledButton } from '../../atoms';
 
 const StyledNavContainer = styled.div`
-  width: 90%;
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  background-color: white;
+  box-shadow: 0 8px 6px -10px black;
+  line-height: 2;
+  margin: 0 auto 5px auto;
+  padding: 0 10px;
 `;
 
-const AppNav = () => (
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
+
+const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  .active {
+    color: #79AC48;
+  }
+`;
+
+const StyledAuthNav = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 150px;
+`;
+
+const StyledRightNav = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 70%;
+`;
+
+const AppNav = ({ loggedIn }) => (
   <StyledNavContainer>
-    <div></div>
-    <Link
+    {/* <div /> */}
+    <StyledLink
       to="/"
     >
       <StyledLogoText>I&apos;ll Serve Soup</StyledLogoText>
-    </Link>
+    </StyledLink>
 
-    <div>
-      <Link
-        to="/login"
-      >
-        <StyledButton withoutBorder>Sign in</StyledButton>
-      </Link>
-      <Link
-        to="/signup"
-      >
-        <StyledButton primary>Get Started</StyledButton>
-      </Link>
-    </div>
+    {
+      loggedIn
+        ? (
+          <StyledRightNav>
+            <StyledNavLink>
+              Inventory
+            </StyledNavLink>
+            <StyledNavLink>
+              Orders
+            </StyledNavLink>
+            <StyledNavLink>
+              Donations
+            </StyledNavLink>
+            <span>|</span>
+            <div>profile</div>
+          </StyledRightNav>
+        )
+        : (
+          <StyledAuthNav>
+            <StyledLink
+              to="/login"
+            >
+              <StyledButton withoutBorder>Sign in</StyledButton>
+            </StyledLink>
+            <StyledLink
+              to="/signup"
+            >
+              <StyledButton tertiary>Get Started</StyledButton>
+            </StyledLink>
+          </StyledAuthNav>
+        )
+    }
   </StyledNavContainer>
 );
 
