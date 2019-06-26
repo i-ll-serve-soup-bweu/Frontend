23bd71b9c6eb6e6d800c4e62a3b2588e1e2260c1
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-26 13:21:09 +0000

    feature: Add sidebar frame

---
+++ serve-soup-inventory-mgt/src/components/organisms/Sidebar/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 200px;
+  height: 100vh;
+  background-color: #F1F1F1;
+`;
+
+const Sidebar = () => (
+  <Wrapper>
+    <p>sds</p>
+  </Wrapper>
+);
+
+export default Sidebar;
