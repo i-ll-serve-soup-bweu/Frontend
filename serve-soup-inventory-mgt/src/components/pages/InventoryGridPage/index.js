042d49f52b244a22b022df41eb628a43bd05088c
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-26 01:35:11 +0100

    feature: Redirect to create Kitchen Form if logged in user has no kitchen

--- serve-soup-inventory-mgt/src/components/pages/InventoryGridPage/index.js
+++ serve-soup-inventory-mgt/src/components/pages/InventoryGridPage/index.js
@@ -1,18 +1,38 @@
 import React, { useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import pt from 'prop-types';
 
 import { doGetKitchen } from '../../../actions';
 import { DisplayInventory, AppNav } from '../../organisms';
 
-function InventoryGrid({ doGetKitchen }) {
+function InventoryGrid({
+  doGetKitchen, kitchen, loadingKitchen, error,
+}) {
   useEffect(() => {
     doGetKitchen();
   }, [doGetKitchen]);
 
+  if (loadingKitchen) {
+    return (
+      <p>loading kitchen</p>
+    );
+  }
+
+  if (!kitchen) {
+    return (
+      <Redirect
+        to="/kitchen/create"
+      />
+    );
+  }
+
   return (
     <div>
       <AppNav loggedIn />
+      {
+        error && <p>{error}</p>
+      }
       <DisplayInventory />
     </div>
   );
@@ -26,6 +46,18 @@ const mapStateToProps = state => ({
 
 export default connect(mapStateToProps, { doGetKitchen })(InventoryGrid);
 
+InventoryGrid.defaultProps = {
+  kitchen: undefined,
+};
+
 InventoryGrid.propTypes = {
   doGetKitchen: pt.func.isRequired,
+  kitchen: pt.shape({
+    kitchen_name: pt.string.isRequired,
+    location: pt.string.isRequired,
+    description: pt.string.isRequired,
+    km_id: pt.number.isRequired,
+  }),
+  loadingKitchen: pt.bool.isRequired,
+  error: pt.string.isRequired,
 };
