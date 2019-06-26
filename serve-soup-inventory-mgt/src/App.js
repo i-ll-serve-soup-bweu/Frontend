fbbd94a8ad224062aba35544feeae0f332fd408e
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-26 12:42:14 +0100

    refactor: Delete unneccessary Pages

--- serve-soup-inventory-mgt/src/App.js
+++ serve-soup-inventory-mgt/src/App.js
@@ -1,57 +1,60 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import PrivateRoute from './components/PrivateRoute';
-import { LoginPage, SignUpPage } from './components/pages';
+import LoginForm from './components/organisms/LoginForm';
+import SignUpForm from './components/organisms/SignUpForm';
+import Header from './components/templates/Header';
 import InventoryGrid from './components/pages/InventoryGridPage';
-import InventoryItemDetails from './components/pages/InventoryDetailPage';
-import InventoryItemForm from './components/pages/InventoryItemPage';
+import InventoryItemDetailForm from './components/organisms/InventoryItemDetailForm';
 import CreateKitchenPage from './components/pages/CreateKitchenPage';
 
 function App() {
   return (
     <Router>
-      <Route
-        exact
-        path="/login"
-        component={LoginPage}
-      />
-      <Route
-        exact
-        path="/signup"
-        component={SignUpPage}
-      />
+      <Header />
+      <Switch>
+        <Route
+          exact
+          path="/login"
+          component={LoginForm}
+        />
+        <Route
+          exact
+          path="/signup"
+          component={SignUpForm}
+        />
 
-      <PrivateRoute
-        exact
-        path={['/', '/inventory']}
-        component={InventoryGrid}
-      />
+        <PrivateRoute
+          exact
+          path={['/', '/inventory']}
+          component={InventoryGrid}
+        />
 
-      <PrivateRoute
-        exact
-        path="/inventory/:id"
-        component={InventoryItemDetails}
-      />
+        <PrivateRoute
+          exact
+          path="/inventory/:id"
+          component={InventoryItemDetailForm}
+        />
 
-      <PrivateRoute
-        exact
-        path="/inventory/add-item"
-        component={InventoryItemForm}
-      />
+        <PrivateRoute
+          exact
+          path="/inventory/add-item"
+          component={InventoryItemDetailForm}
+        />
 
-      <PrivateRoute
-        exact
-        path="/inventory/:id/edit-item"
-        component={InventoryItemForm}
-      />
+        <PrivateRoute
+          exact
+          path="/inventory/:id/edit-item"
+          component={InventoryItemDetailForm}
+        />
 
-      <PrivateRoute
-        exact
-        path="/kitchen/create"
-        component={CreateKitchenPage}
-      />
-
+        <PrivateRoute
+          exact
+          path="/kitchen/create"
+          component={CreateKitchenPage}
+        />
+      </Switch>
     </Router>
   );
 }
