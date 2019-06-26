723114cfb17c7dcc63cbec7581c450789a41b4f7
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-26 13:50:03 +0000

    chore: Export CreateKitchenPage as default

--- serve-soup-inventory-mgt/src/components/pages/index.js
+++ serve-soup-inventory-mgt/src/components/pages/index.js
@@ -3,3 +3,4 @@ export { default as InventoryPage } from './InventoryGridPage';
 export { default as InventoryItemPage } from './InventoryItemPage';
 export { default as LoginPage } from './LoginPage';
 export { default as SignUpPage } from './SignUpPage';
+export { default as CreateKitchenPage } from './CreateKitchenPage';
