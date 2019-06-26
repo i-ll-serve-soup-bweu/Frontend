8aaf4079b91dbe319f560fac28045ab463984f06
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-06-26 07:32:22 +0100

    feature: Connect CreateKitchenPage to store make doAddKitchen available for CreateKitchenPage

--- serve-soup-inventory-mgt/src/components/pages/CreateKitchenPage/index.js
+++ serve-soup-inventory-mgt/src/components/pages/CreateKitchenPage/index.js
@@ -1,24 +1,64 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import pt from 'prop-types';
 import { history as historyPropTypes } from 'history-prop-types';
 
+import { doGetKitchen } from '../../../actions';
 import { CreateKitchenForm } from '../../organisms';
 import { ExternalTemplate } from '../../templates';
 
-export default function CreateKitchenPage({ history, location, match }) {
+function CreateKitchenPage({
+  history, kitchen, doGetKitchen, loadingKitchen,
+}) {
+  useEffect(() => {
+    doGetKitchen();
+  }, [doGetKitchen]);
+
+  if (loadingKitchen) {
+    return (
+      <p>loading kitchen</p>
+    );
+  }
+  if (kitchen) {
+    return (
+      <Redirect
+        to="/"
+      />
+    );
+  }
   return (
     <div>
       <ExternalTemplate>
         <CreateKitchenForm
           history={history}
-          location={location}
-          match={match}
         />
       </ExternalTemplate>
     </div>
   );
 }
 
+const mapStateToProps = state => ({
+  kitchen: state.user.kitchen,
+  loadingKitchen: state.user.loadingKitchen,
+});
+
+export default connect(mapStateToProps, { doGetKitchen })(CreateKitchenPage);
+
+
+CreateKitchenPage.defaultProps = {
+  kitchen: undefined,
+};
+
 CreateKitchenPage.propTypes = {
+  doGetKitchen: pt.func.isRequired,
+  kitchen: pt.shape({
+    id: pt.number,
+    kitchen_name: pt.string,
+    location: pt.string,
+    description: pt.string,
+    km_id: pt.number,
+  }),
+  loadingKitchen: pt.bool.isRequired,
   history: pt.shape(historyPropTypes).isRequired,
 };
