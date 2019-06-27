5fed736d426403ab44c7e8bd3485018f641202aa
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-27 14:41:42 +0000

    refactor: Improve layout

--- serve-soup-inventory-mgt/src/components/organisms/DisplayInventory/index.js
+++ serve-soup-inventory-mgt/src/components/organisms/DisplayInventory/index.js
@@ -8,8 +8,20 @@ import styled from 'styled-components';
 import { doGetInventory, doDeleteItem } from '../../../actions';
 import { Table } from '../../molecules';
 import {
-  TableCell, TableHead, TableRow, StyledInput, LoaderContainer,
+  TableCell, TableHead, TableRow, LoaderContainer, StyledHeading, StyledActionButton, Arrow,
 } from '../../atoms';
+
+const StyledDisplayInventoryContainer = styled.div`
+  margin-top: 16px;
+  padding-left: 10px;
+  width: 100%;
+`;
+
+const StyledHeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 const StyledBadge = styled.span`
   display: inline-block;
@@ -43,55 +55,85 @@ const DisplayInventory = ({
     );
   }
 
-  if (!inventory[0]) {
-    return (
-      <h3>
-        No Inventory Items added yet.
-        {' '}
-        <Link to="/inventory/add-item">Click here</Link>
-        {' '}
-        to add inventory items
-      </h3>
-    );
-  }
-  return (
 
-    <Table>
-      <thead>
-        <TableRow>
-          <TableHead><StyledInput type="checkbox" /></TableHead>
-          <TableHead>Item Name</TableHead>
-          <TableHead>Stock</TableHead>
-          <TableHead>Category</TableHead>
-          <TableHead />
-        </TableRow>
-      </thead>
-      <tbody>
-        {
-          inventory.map(item => (
-            <TableRow key={item.id}>
-              <TableCell><StyledInput type="radio" /></TableCell>
-              <TableCell>
-                {`${item.item_name}`}
-                {!item.quantity ? (<StyledBadge>Out of Stock</StyledBadge>) : ''}
-              </TableCell>
-              <TableCell>{`${item.quantity} ${item.measurement_unit}`}</TableCell>
-              <TableCell>{item.category}</TableCell>
-              <TableCell>
-                <Link to={`/inventory/${item.id}`}>Click</Link>
-              </TableCell>
-              <TableCell>
-                <StyledDelete
-                  onClick={() => onDeleteItem(item.id)}
-                  src="https://image.flaticon.com/icons/svg/1214/1214594.svg"
-                  alt="delete"
-                />
-              </TableCell>
-            </TableRow>
-          ))
-        }
-      </tbody>
-    </Table>
+  return (
+    <StyledDisplayInventoryContainer>
+      <StyledHeaderContainer>
+        <StyledHeading secondary>Inventory</StyledHeading>
+        <Link to="/inventory/add-item"><StyledActionButton>+</StyledActionButton></Link>
+      </StyledHeaderContainer>
+
+      <Table>
+        <thead>
+          <TableRow>
+            <TableHead>Item Name</TableHead>
+            <TableHead
+              style={{ textAlign: 'center' }}
+            >
+              Stock
+            </TableHead>
+            <TableHead
+              style={{ textAlign: 'right' }}
+            >
+              Category
+            </TableHead>
+            <TableHead
+              style={{ width: '50px' }}
+            />
+            <TableHead
+              style={{ width: '30px' }}
+            />
+          </TableRow>
+        </thead>
+        <tbody>
+          {
+            !inventory[0] && (
+              <StyledHeading info>
+                No Inventory Items added yet.
+                  {' '}
+                <Link to="/inventory/add-item">Click here</Link>
+                {' '}
+                to add inventory items
+              </StyledHeading>
+            )
+          }
+          {
+            inventory.map(item => (
+              <TableRow key={item.id}>
+                <TableCell>
+                  {`${item.item_name}`}
+                  {!item.quantity ? (<StyledBadge>Out of Stock</StyledBadge>) : ''}
+                </TableCell>
+                <TableCell
+                  style={{ textAlign: 'center' }}
+                >
+                  {`${item.quantity} ${item.measurement_unit}`}
+                </TableCell>
+                <TableCell
+                  style={{ textAlign: 'right' }}
+                >
+                  {item.category}
+                </TableCell>
+                <TableCell
+                  style={{ textAlign: 'center' }}
+                >
+                  <Link to={`/inventory/${item.id}`}><Arrow right /></Link>
+                </TableCell>
+                <TableCell
+                  style={{ textAlign: 'center' }}
+                >
+                  <StyledDelete
+                    onClick={() => onDeleteItem(item.id)}
+                    src="https://image.flaticon.com/icons/svg/1214/1214594.svg"
+                    alt="delete"
+                  />
+                </TableCell>
+              </TableRow>
+            ))
+          }
+        </tbody>
+      </Table>
+    </StyledDisplayInventoryContainer>
   );
 };
 
