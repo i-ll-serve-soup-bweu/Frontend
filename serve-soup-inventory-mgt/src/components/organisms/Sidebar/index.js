a6ba48514357b1206c46f8e10bd1c64a0b2f10f4
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-27 06:41:13 +0000

    feature: Create and export sidebar

--- serve-soup-inventory-mgt/src/components/organisms/Sidebar/index.js
+++ serve-soup-inventory-mgt/src/components/organisms/Sidebar/index.js
@@ -1,15 +1,43 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 const Wrapper = styled.div`
-  width: 200px;
+  /* width: 200px; */
   height: 100vh;
   background-color: #F1F1F1;
+  padding-top: 30px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  color: inherit;
+  font-size: 16px;
+  font-weight: 300;
+  margin: 0;
+  padding: 10px 8px;
+  
+  &.active {
+    background-color: white !important;
+  }
 `;
 
 const Sidebar = () => (
   <Wrapper>
-    <p>sds</p>
+    <StyledNavLink
+      to="/notifications"
+      activeClassName="active"
+    >
+      Notifications
+    </StyledNavLink>
+    <StyledNavLink
+      to="/inventory"
+      activeClassName="active"
+    >
+      Inventory
+    </StyledNavLink>
   </Wrapper>
 );
 
--- serve-soup-inventory-mgt/src/components/organisms/Sidebar/index.stories.js
+++ serve-soup-inventory-mgt/src/components/organisms/Sidebar/index.stories.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Sidebar from '.';
 
 storiesOf('Sidebar', module)
-  .add('default', () => <Sidebar />);
+  .add('default', () => (
+    <Router>
+      <Sidebar />
+    </Router>
+  ));
