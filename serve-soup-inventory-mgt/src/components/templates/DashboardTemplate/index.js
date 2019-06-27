f6526a4ff65eb1d796cb5d551f5a8e50591fecdf
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-27 06:34:22 +0000

    feature: Create design for dashboard

---
+++ serve-soup-inventory-mgt/src/components/templates/DashboardTemplate/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { AppNav } from '../../organisms';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  box-sizing: border-box;
+`;
+
+const Main = styled.div`
+  margin: 20px auto;
+  display: flex;
+  justify-content: column;
+`;
+
+// const ContentArea = styled.div`
+//   width: 700px;
+// `;
+
+const DashboardTemplate = ({ children }) => (
+  <Wrapper>
+    <AppNav loggedIn />
+    <Main>
+      {children}
+    </Main>
+  </Wrapper>
+);
+
+export default DashboardTemplate;
