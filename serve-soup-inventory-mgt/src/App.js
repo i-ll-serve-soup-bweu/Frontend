145865168b76fac3d29ae450d827d75f4c118630
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-27 06:32:16 +0000

    feature: Add routes for dashboard content area

--- serve-soup-inventory-mgt/src/App.js
+++ serve-soup-inventory-mgt/src/App.js
@@ -1,50 +1,62 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
 
 import PrivateRoute from './components/PrivateRoute';
+import { Sidebar, DisplayInventory, InventoryItemForm } from './components/organisms';
 import { LoginPage, SignUpPage, CreateKitchenPage } from './components/pages';
-import InventoryGrid from './components/pages/InventoryGridPage';
-import InventoryItemDetailForm from './components/organisms/InventoryItemDetailForm';
+import { DashboardTemplate } from './components/templates';
+
+const StyledSidebar = styled.div`
+  width: 200px;
+`;
+
+const StyledDashboardContent = styled.div`
+  width: 600px;
+`;
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route
-          exact
-          path="/login"
-          component={LoginPage}
-        />
-        <Route
-          exact
-          path="/signup"
-          component={SignUpPage}
-        />
-
-        <PrivateRoute
-          exact
-          path={['/', '/inventory']}
-          component={InventoryGrid}
-        />
-
-        <PrivateRoute
-          exact
-          path="/inventory/:id"
-          component={InventoryItemDetailForm}
-        />
-
-        <PrivateRoute
-          exact
-          path="/inventory/add-item"
-          component={InventoryItemDetailForm}
-        />
-
-        <PrivateRoute
-          exact
-          path="/kitchen/create"
-          component={CreateKitchenPage}
-        />
-      </Switch>
+      <Route
+        exact
+        path="/login"
+        component={LoginPage}
+      />
+      <Route
+        exact
+        path="/signup"
+        component={SignUpPage}
+      />
+      <PrivateRoute
+        exact
+        path="/kitchen/create"
+        component={CreateKitchenPage}
+      />
+      <DashboardTemplate>
+        <StyledSidebar>
+          <Sidebar />
+        </StyledSidebar>
+        <StyledDashboardContent>
+          <Switch>
+            <PrivateRoute
+              exact
+              path={['/', '/inventory']}
+              component={DisplayInventory}
+            />
+            <PrivateRoute
+              exact
+              path="/inventory/:id"
+              component={InventoryItemForm}
+            />
+            <PrivateRoute
+              exact
+              path="/inventory/add-item"
+              component={InventoryItemForm}
+            />
+          </Switch>
+        </StyledDashboardContent>
+      </DashboardTemplate>
     </Router>
   );
 }
